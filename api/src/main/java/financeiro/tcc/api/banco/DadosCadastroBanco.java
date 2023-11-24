4f505425a706cef47116d545a14a5ca4dcced922
Euder Alves <euder.martinsalves@emeal.nttdata.com>
2023-11-24 18:00:00 -0300

    conectar com banco railway

---
+++ api/src/main/java/financeiro/tcc/api/banco/DadosCadastroBanco.java
@@ -0,0 +1,4 @@
+package financeiro.tcc.api.banco;
+
+public record DadosCadastroBanco( String nomeBanco, Long numeroConta, Long agencia, String tipoConta) {
+}
--- api/src/main/java/financeiro/tcc/api/controller/ContasBancarias.java
+++ api/src/main/java/financeiro/tcc/api/controller/ContasBancarias.java
@@ -1,4 +1,17 @@
 package financeiro.tcc.api.controller;
 
+import financeiro.tcc.api.banco.DadosCadastroBanco;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("contasBancarias")
 public class ContasBancarias {
+
+    @PostMapping()
+    public void cadastrarContas(@RequestBody DadosCadastroBanco dadosCadastroBanco) {
+        System.out.println((dadosCadastroBanco));
+    }
 }
