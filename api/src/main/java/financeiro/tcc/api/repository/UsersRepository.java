e205fd812bb6886d11c17006ecaa163f8ee095ba
Euder Alves <euder.martinsalves@emeal.nttdata.com>
2023-11-30 21:15:24 -0300

    correcao query user

--- api/src/main/java/financeiro/tcc/api/repository/UsersRepository.java
+++ api/src/main/java/financeiro/tcc/api/repository/UsersRepository.java
@@ -2,9 +2,14 @@ package financeiro.tcc.api.repository;
 
 import financeiro.tcc.api.models.users.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UsersRepository extends JpaRepository<Users, Long> {
+
+    @Query(name = """
+            SELECT  * FROM users u\s
+            WHERE u.email  = :email""",nativeQuery = true)
     Optional<Users> findByEmail(String email);
 }
